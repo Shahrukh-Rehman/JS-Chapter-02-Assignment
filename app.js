2d5108f6d3419beae1fb1783277cd79a3ebe5912
Shahrukh-Rehman <shahrukhrehman321@gmail.com>
2023-09-10 00:06:52 +0500

    Question 1 to 8

---
+++ app.js
@@ -0,0 +1,27 @@
+//1
+// var username
+//2
+// var myName= "Shahrukh Rehman"
+//3
+// var message
+// message="Hello World"
+// alert(message)
+//4
+// var name= "john Doe"
+// var age= "15 Years old"
+// var certification="Certified Mobile Application Development"
+// alert(name)
+// alert(age)
+// alert(certification)
+//5
+// var pizza="Pizza\nPizz\nPiz\nPi\nP"
+// alert(pizza)
+//6
+// var email="shahrukrehman022@gmail.com"
+// alert("My email address is " + email) 
+//7
+// var book="A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book)
+//8
+// document.write("Yah! I can write HTML content through JavaScript")
+
